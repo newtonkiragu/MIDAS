593f5dfcecd55e8f287de34267f662bb0a3a4ad2
newtonkiragu <karanunewton4@gmail.com>
2020-08-28 20:39:06 +0300

    renamed variables --WIP

--- app/upload.php
+++ app/upload.php
@@ -1,23 +1,24 @@
 <?php
 if (isset($_POST['submit'])) {
-	$file = $_FILES['file'];
-	$fileName = $_FILES['file']['name'];
-	$fileTmpName = $_FILES['file']['tmp_name'];
-	$fileSize = $_FILES['file']['size'];
-	$fileError = $_FILES['file']['error'];
-	$fileType = $_FILES['file']['type'];
+//	apl stands for application letter
+	$apl = $_FILES['file'];
+	$aplName = $_FILES['file']['name'];
+	$aplTmpName = $_FILES['file']['tmp_name'];
+	$aplSize = $_FILES['file']['size'];
+	$aplError = $_FILES['file']['error'];
+	$aplType = $_FILES['file']['type'];
 
-	$fileExt = explode('.', $fileName);
-	$fileActualExt = strtolower(end($fileExt));
+	$aplExt = explode('.', $aplName);
+	$aplActualExt = strtolower(end($aplExt));
 
 	$allowed = array('doc', 'docx', 'pdf', 'jpeg', 'png', 'jpg');
 
-	if (in_array($fileActualExt, $allowed)) {
-		if($fileError === 0){
-			if($fileSize < 1500000){
-				$fileNameNew = uniqid('',true).".".$fileActualExt;
-				$fileDestination = 'upload/'.$fileNameNew;
-				move_uploaded_file(	$fileTmpName, $fileDestination);
+	if (in_array($aplActualExt, $allowed)) {
+		if($aplError === 0){
+			if($aplSize < 1500000){
+				$aplNameNew = uniqid('',true).".".$aplActualExt;
+				$aplDestination = 'upload/'.$aplNameNew;
+				move_uploaded_file(	$aplTmpName, $aplDestination);
 				header('location: htpp://midas/app');
 			}else{
 				echo "your file is to big!";
@@ -30,7 +31,7 @@ if (isset($_POST['submit'])) {
 	}
 } 
 
-  $a = $fileNameNew;
+  $a = $aplNameNew;
   
   $servername = "localhost";
   $username = "root";
